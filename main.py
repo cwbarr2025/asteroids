1eb82724e51f3abbbaf7d9c6417430e4c75d9594
cwbarr <cameronbarr0114@gmail.com>
2025-11-02 05:26:07 -0700

    add clock

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ def main():
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock() #create clock object to help manage framerate and keep track of game time
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60) # set framerate to 60 fps
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
