5c6d3d03f861f1be486eadf3fe6d99359cfeda81
cwbarr <cameronbarr0114@gmail.com>
2025-11-04 13:01:42 -0700

    draw player1 triangle in middle of screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,11 +11,14 @@ def main():
     clock = pygame.time.Clock() #create clock object to help manage framerate and keep track of game time
     dt = 0
 
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         clock.tick(60) # set framerate to 60 fps
         dt = clock.tick(60) / 1000
