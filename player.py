d7a182da54ce7454a471f4b61f7ed1772b356d9b
cwbarr <cameronbarr0114@gmail.com>
2025-11-04 13:27:36 -0700

    add forward and backward movement with w and s keys

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS , PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS , PLAYER_TURN_SPEED , PLAYER_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -8,8 +8,8 @@ class Player(CircleShape):
         self.rotation = 0
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+        right = pygame.Vector2(0, -1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
@@ -28,3 +28,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(-dt)
+        if keys[pygame.K_s]:
+            self.move(dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
