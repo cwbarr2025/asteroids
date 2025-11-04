8b2bc2457404f17779c3658b755cc71efc93475a
cwbarr <cameronbarr0114@gmail.com>
2025-11-04 13:15:21 -0700

    make a and d keys rotate player left and right resptectively

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player1.update(dt)
         screen.fill("black")
         player1.draw(screen)
         pygame.display.flip()
